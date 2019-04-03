f8d142a391c020625bc513a44fad7a2a5eb30d33
jlherdezz <urferu@gmail.com>
2019-04-02 19:53:07 -0700

    Se reduce a 20 fields

--- Core/Commands/ChannelCommands/ListGames.cs
+++ Core/Commands/ChannelCommands/ListGames.cs
@@ -11,18 +11,40 @@ namespace DriveBot.Core.Commands
         [Command("list"), Alias("lista", "dame la lista de juegos", "que juegos tienes?"), Summary("Lista de juegos")]
         public async Task sJustein([Remainder]string inputMessage = "NONE")
         {
-            if (!Context.IsPrivate || stdClassCSharp.readJsonFile("usersIds.json")["users"][Context.User.Id.ToString(), TiposDevolver.Boleano])
+            try
             {
-                var builder = new EmbedBuilder();
-                int fields = 0;
-                string listaLetraAnterior = "";
-                string letraBuscar = inputMessage.Length == 1 ? inputMessage : "";
-                builder.WithTitle("Estos son los juegos que tengo disponibles:");
-                //.WithDescription(Utils.getListGames())
-                builder.WithColor(new Color(0xAF3A));
-                foreach(string listaLetra in Utils.getListGames(letraBuscar))
+                if (!Context.IsPrivate || stdClassCSharp.readJsonFile("usersIds.json")["users"][Context.User.Id.ToString(), TiposDevolver.Boleano])
                 {
-                    if(fields >= 24)
+                    var builder = new EmbedBuilder();
+                    int fields = 0;
+                    string listaLetraAnterior = "";
+                    string letraBuscar = inputMessage.Length == 1 ? inputMessage : "";
+                    builder.WithTitle("Estos son los juegos que tengo disponibles:");
+                    //.WithDescription(Utils.getListGames())
+                    builder.WithColor(new Color(0xAF3A));
+                    foreach (string listaLetra in Utils.getListGames(letraBuscar))
+                    {
+                        if (fields >= 20)
+                        {
+                            if (inputMessage.ToLower().Contains("aqui"))
+                                await Context.Channel.SendMessageAsync("", false, builder.Build());
+                            else
+                                await Context.User.SendMessageAsync("", false, builder.Build());
+
+                            builder = new EmbedBuilder();
+                            builder.WithColor(new Color(0xAF3A));
+                            fields = 0;
+                        }
+                        if (listaLetra.Substring(0, 1) == listaLetraAnterior)
+                            builder.AddField($"{listaLetra.Substring(0, 1)} continuación", listaLetra);
+                        else
+                            builder.AddField(listaLetra.Substring(0, 1), listaLetra);
+
+                        fields++;
+                        listaLetraAnterior = listaLetra.Substring(0, 1);
+                    }
+
+                    if (fields >= 20)
                     {
                         if (inputMessage.ToLower().Contains("aqui"))
                             await Context.Channel.SendMessageAsync("", false, builder.Build());
@@ -33,58 +55,43 @@ namespace DriveBot.Core.Commands
                         builder.WithColor(new Color(0xAF3A));
                         fields = 0;
                     }
-                    if(listaLetra.Substring(0, 1) == listaLetraAnterior)
-                        builder.AddField($"{listaLetra.Substring(0, 1)} continuación", listaLetra);
-                    else
-                        builder.AddField(listaLetra.Substring(0, 1), listaLetra);
 
-                    fields++;
-                    listaLetraAnterior = listaLetra.Substring(0, 1);
-                }
+                    builder.AddField("¿Como pedir juegos?", $"Puedes pedirme juegos, updates y dlc solamente buscando" +
+                        $" por una parte de su titulo de la siguiente manera:{Environment.NewLine}" +
+                        $"Para pedirme un juego puedes utilizar el comando (!game juego_que_deseas) o puedes mencionarme y" +
+                        $" agregar las frases (juego juego_que_deseas), (si el juego_que_deseas) o (quiero el juego_que_deseas).{Environment.NewLine}{Environment.NewLine}" +
+                        $"Para pedirme una actualización o update puedes utilizar el comando (!update nombre_juego_del_update) o puedes mencionarme y agregar las frases" +
+                        $" (update nombre_juego_del_update), (quiero el update del nombre_juego_del_update), (actualizacion nombre_juego_del_update), (actualización nombre_juego_del_update) o (quiero la actualizacion del nombre_juego_del_update)" +
+                        $".{Environment.NewLine}{Environment.NewLine}Para pedirme los dlc's de un juego puedes utilizar el comando (!dlc nombre_juego_del_dlc) o puedes mencionarme " +
+                        $"y agregar las frases (dlc nombre_juego_del_dlc), (ocupo el dlc del nombre_juego_del_dlc), (ocupo los dlc del nombre_juego_del_dlc) o (quiero los dlc del nombre_juego_del_dlc)." +
+                        $"{Environment.NewLine}{Environment.NewLine} Espero te sea mas comodo pedirme tus juegos.");
 
-                if (fields >= 24)
-                {
+
                     if (inputMessage.ToLower().Contains("aqui"))
+                    {
+                        if (!Context.IsPrivate)
+                            await Context.Message.DeleteAsync(RequestOptions.Default);
                         await Context.Channel.SendMessageAsync("", false, builder.Build());
+                    }
                     else
+                    {
+                        if (!Context.IsPrivate)
+                        {
+                            await Context.Message.DeleteAsync(RequestOptions.Default);
+                            await Context.Channel.SendMessageAsync($"{Context.Message.Author.Mention} he respondido tu solicitud por mp :)");
+                        }
                         await Context.User.SendMessageAsync("", false, builder.Build());
 
-                    builder = new EmbedBuilder();
-                    builder.WithColor(new Color(0xAF3A));
-                    fields = 0;
-                }
-
-                builder.AddField("¿Como pedir juegos?", $"Puedes pedirme juegos, updates y dlc solamente buscando" +
-                    $" por una parte de su titulo de la siguiente manera:{Environment.NewLine}" +
-                    $"Para pedirme un juego puedes utilizar el comando (!game juego_que_deseas) o puedes mencionarme y" +
-                    $" agregar las frases (juego juego_que_deseas), (si el juego_que_deseas) o (quiero el juego_que_deseas).{Environment.NewLine}{Environment.NewLine}" +
-                    $"Para pedirme una actualización o update puedes utilizar el comando (!update nombre_juego_del_update) o puedes mencionarme y agregar las frases" +
-                    $" (update nombre_juego_del_update), (quiero el update del nombre_juego_del_update), (actualizacion nombre_juego_del_update), (actualización nombre_juego_del_update) o (quiero la actualizacion del nombre_juego_del_update)" +
-                    $".{Environment.NewLine}{Environment.NewLine}Para pedirme los dlc's de un juego puedes utilizar el comando (!dlc nombre_juego_del_dlc) o puedes mencionarme " +
-                    $"y agregar las frases (dlc nombre_juego_del_dlc), (ocupo el dlc del nombre_juego_del_dlc), (ocupo los dlc del nombre_juego_del_dlc) o (quiero los dlc del nombre_juego_del_dlc)." +
-                    $"{Environment.NewLine}{Environment.NewLine} Espero te sea mas comodo pedirme tus juegos.");
-
-
-                if (inputMessage.ToLower().Contains("aqui"))
-                {
-                    if(!Context.IsPrivate)
-                        await Context.Message.DeleteAsync(RequestOptions.Default);
-                    await Context.Channel.SendMessageAsync("", false, builder.Build());
+                    }
                 }
                 else
                 {
-                    if (!Context.IsPrivate)
-                    {
-                        await Context.Message.DeleteAsync(RequestOptions.Default);
-                        await Context.Channel.SendMessageAsync($"{Context.Message.Author.Mention} he respondido tu solicitud por mp :)");
-                    }
-                    await Context.User.SendMessageAsync("", false, builder.Build());
-                    
+                    await Context.User.SendMessageAsync("Este comando no funciona por mp o no estas autorizado para utilizarlo.");
                 }
             }
-            else
+            catch(Exception ex)
             {
-                await Context.User.SendMessageAsync("Este comando no funciona por mp o no estas autorizado para utilizarlo.");
+                await Context.User.SendMessageAsync($"Ocurrió un error al completar tu solicitud comunicale a urferu el siguiente error: {ex.Message}", false);
             }
         }
     }
